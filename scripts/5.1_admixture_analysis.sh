4f60f0a23c0fe4d5e36b88c90e5d4468fed6b899
Ariana Cerreta <ariana.cerreta@tamuk.edu>
2026-09-22 12:06:19 -0500

    update 5.1_admixture to general paths

--- scripts/5.1_admixture_analysis.sh
+++ scripts/5.1_admixture_analysis.sh
@@ -2,9 +2,15 @@
 
 #using LD pruned dataset: wild_LDpruned_05
 
-#setting working directory to folder with admixture
-cd /Users/tylerbostwick/Documents/Masters_Work/Analyses/ADMIXTURE/dist/admixture_macosx-1.3.0
+#setting paths to folder with admixture
+export ADMIXTURE="/path/admixture_macosx-1.3.0" #update directory to your executable admixture
+export INPUT_FILE="./data/processed/addtl_filter/wild_LDpruned_05.bed" #no need to change if forked from GitHub
+export OUTPUT_DIR="./results/admixture" #no need to change if forked from GitHub
+
+#make new output dir
+mkdir -p "$OUTPUT_DIR"
+
 #running admixture
-  for k in {2..5}; do ./admixture --cv=10 wild_LDpruned_05.bed $k > wild_LDpruned_05k${k}.txt; done
+  for k in {2..5}; do "${ADMIXTURE}"/admixture --cv=10 "$INPUT_FILE" $k > "${OUTPUT_DIR}"/wild_LDpruned_05k${k}.txt; done
 
 #runs the admixture program for k 2-5, generating cross-validation, and writing it to a txt file for each k ran
